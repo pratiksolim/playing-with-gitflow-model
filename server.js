e3d88f8ff21b978d11654299c0341ed633306c2e
Pratik Solim <pratiksolim@users.noreply.github.com>
2021-05-20 23:45:56 +0530

    [feature/health-check] Added  health-check API (#1)
    * Initialized Node project
    * Added express server with health-check endpoint
    * Added gitignore file

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/ping', (_req, res) => {
+  res.status(200).json({
+    message: 'Health-check successful'
+  })
+
+  return
+})
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080')
+})
